d97683c32d179e9431c59904872793e356eb561e
ZNYYY <295266301@qq.com>
2018-12-05 18:21:14 +0800

    更新

--- module1/test.py
+++ module1/test.py
@@ -2,22 +2,29 @@
 # @Time    : 2018/11/24 9:49
 # @Author  : Xiao
 
-#格式化输出，就是设计好套路，在输出的时候按这个套路来输出,主要有四种
-name='进击的菜鸟'
-money=100
-#用+号连接
-print("Welcome to 'From zero single row':"+name)
-#用,号连接
-print("Welcome to 'From zero single row':",name)
-#用百分号占位符,%s只是先占了一个位置在这里，然后再输出的时候把后面的值依次填充到占位的位置
-print("Welcome to 'From zero single row':[%s]! Let's study!"%name)
-#多个占位时，这样用，需要括弧括起来，必须是按照占位的顺序才可以
-print("Welcome to 'From zero single row':[%s]! Your have money:[%s]元"%(name,money))
-#拓展，%s占位的是字符类型的位置，%d占位的是整数类型的位置，%.2f占位的是小数类型(会四舍五入)的位置
-print("Welcome to 'From zero single row':[%s]! Your have money:[%d]元!Your height is[%.2f]cm"%(name,money,180.235))
-#用format格式，这个格式，就不用担心顺序问题，但参数夺得时候用这个
-print("Welcome to 'From zero single row':[{name}]! 'Your have money:[{money}]元'".format(money=money,name=name))
-#format传list或字典需要加*号，如字典：
-dic={"name":"Xiao","age":18}
-print("My name is {name},I'm {age} years old.".format(**dic))
-#练习，输入名字和年龄，然后格式化打印：你的名字是：xxx，你的年龄是：xx。
+
+lis = [{"k1":"v1","k2":"v2"},{"k3":"v3","k4":"v4"}]
+
+def store(file_name,lis):
+    lis_new = []
+    for i in lis:
+        lis_new.append(str(i).replace(":","/").replace(",",";").replace("{","").replace("}","").replace("'","").replace(" ",""))
+    lis_str = r'\n'.join(lis_new)
+    with open(file_name,"w",encoding="utf-8") as f:
+        f.write(lis_str)
+
+def load(file_name):
+    lis = []
+    with open(file_name, "r", encoding="utf-8") as f:
+        tmp_lis = f.read().split(r"\n")
+        for i in tmp_lis:
+            for k in i.split(";"):
+                tmp = k.split(r"/")
+                tmp_dic = {}
+                tmp_dic[tmp[0]] = tmp[1]
+                lis.append(tmp_dic)
+    return lis
+
+store("b.txt",lis)
+new_lis = load("b.txt")
+print(type(new_lis),new_lis)
--- module2/module/moudle_practice.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2018/11/28 8:12
-# @Author  : Xiao
-
