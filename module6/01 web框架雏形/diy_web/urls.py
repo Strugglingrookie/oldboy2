db0c037eba2c1e2ddcca688fe25d37b4c9088f16
ZNYYY <295266301@qq.com>
2019-07-16 15:40:43 +0800

    更新moudle6-django

--- module6/01 web框架雏形/diy_web/urls.py
+++ module6/01 web框架雏形/diy_web/urls.py
@@ -2,3 +2,11 @@
 # @Time    : 2019/7/9 20:05
 # @Author  : Xiao
 
+from views import *
+
+url_datas = [
+    ('/favicon.ico',favicon),
+    ('/index',index),
+    ('/login',login),
+    ('/regist',regist)
+]
