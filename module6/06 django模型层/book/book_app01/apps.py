c91565d397ba0550472fd160089827e9a6b86e2e
ZNYYY <295266301@qq.com>
2019-08-06 20:19:48 +0800

    module6

---
+++ module6/06 django模型层/book/book_app01/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BookApp01Config(AppConfig):
+    name = 'book_app01'
---
+++ module6/06 django模型层/book/book_app01/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path,re_path,include
+
+urlpatterns = [
+    re_path('^book/', ),
+]
