4449ac1d4c625b3924761d4db0c6d45051bb86a3
ZNYYY <295266301@qq.com>
2019-08-10 23:00:55 +0800

    module6

---
+++ module6/06 django模型层/tables/tables_app001/urls.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# __author__="X1gang"
+# Date:2019/8/10
+
+
+from django.urls import path
+from tables_app001 import views
+
+
+urlpatterns = [
+    path('add', views.add),
+    path('query/son', views.query_son),
+
+]
