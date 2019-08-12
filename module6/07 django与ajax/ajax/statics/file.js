fb3c54b3dc3d277cde586f63a518e906932f5fd8
ZNYYY <295266301@qq.com>
2019-08-12 21:45:48 +0800

    module6

--- module6/07 django与ajax/ajax/statics/file.js
+++ module6/07 django与ajax/ajax/statics/file.js
@@ -1,14 +1,17 @@
 $('#ajax_btn').click(function (e) {
     e.preventDefault();
-    var formdata = new FormData();  // 文件必须要以 FormData 数据类型传递，不可以再用默认的application/x-www-form-urlencoded
+    // 同form表单文件请求一样，原默认的 application/x-www-form-urlencoded 类型不支持文件上传，需要转换请求参数类型
+    // 文件必须要以 FormData 数据类型传递
+    var formdata = new FormData();
     formdata.append('name', $('#fileajax').val());
     formdata.append('imgfile', $('#ajax_file')[0].files[0]);
     $.ajax({
         url: '',
         type: 'post',
         data: formdata,
-        processData: false,    // 不处理数据
-        contentType: false,    // 不设置内容类型
+        // 传FormData时 必须设置下面两个参数，不给参数，会报错(js非法输入)
+        processData: false,    // 不处理数据，让FormData自己去处理数据
+        contentType: false,    // 不设置内容类型，让FormData自己设置内容类型
         success: function (data) {
             console.log(data)
         }
