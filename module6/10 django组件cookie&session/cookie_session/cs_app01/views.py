317015c38a0c7a02870baf1a5e8cdbc3964430e8
ZNYYY <295266301@qq.com>
2019-08-16 14:36:24 +0800

    module6

--- module6/10 django组件cookie&session/cookie_session/cs_app01/views.py
+++ module6/10 django组件cookie&session/cookie_session/cs_app01/views.py
@@ -132,12 +132,11 @@ class HttpResponseBase:
         httponly=False       只能http协议传输，无法被JavaScript获取（不是绝对，底层抓包可以获取到也可以被覆盖）
             ): pass
             
+# settings
 SESSION_COOKIE_NAME= "sessionid"                       # Session的cookie保存在浏览器上时的key，即：sessionid＝随机字符串（默认）
 SESSION_COOKIE_PATH= "/"                               # Session的cookie保存的路径（默认）
 SESSION_COOKIE_AGE = 10                             # Session的cookie失效日期（2周）（默认）
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False                # 是否关闭浏览器使得Session过期（默认）
 SESSION_SAVE_EVERY_REQUEST = False                     # 是否每次请求都保存Session，默认修改之后才保存（默认）
-
-            
-            
+      
 '''
