6cea0d8475ed2c2474fd892e4da31a414847f95a
ZNYYY <295266301@qq.com>
2019-08-23 17:33:43 +0800

    module6

--- module6/13 BBS&BLOG/blog/blog_app01/admin.py
+++ module6/13 BBS&BLOG/blog/blog_app01/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from blog_app01 import models
+admin.site.register(models.UserInfo)
+admin.site.register(models.UserBlog)
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Article)
+admin.site.register(models.ArticleUpDown)
+admin.site.register(models.Article2Tag)
+admin.site.register(models.Comment)
