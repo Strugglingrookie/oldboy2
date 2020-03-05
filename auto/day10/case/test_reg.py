debf1cc5c59860368525da082668a599b2b6d0a8
杨晓钢 <295266301@qq.com>
2020-03-05 18:05:50 +0800

    前端模板框架

--- auto/day10/case/test_reg.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestReg(unittest.TestCase):
-    def test_buy(self):
-        self.assertEqual(1, 1, msg='成功')
-
-    def test_buy2(self):
-        self.assertEqual(1, 2, '失败')
